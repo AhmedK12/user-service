7898696a9501c99bef7ce450b6d016226b814e4b
Kamaran Ahmed <kamaran.ahmed@deeptek.ai>
2025-04-03 22:20:27 +0530

    Adding role and permission entity

---
+++ src/main/java/com/healthaiharbor/ai/userservice/modal/Permission.java
@@ -0,0 +1,24 @@
+package com.healthaiharbor.ai.userservice.modal;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "permissions")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "permissions")
+    private Set<Role> roles;
+}
---
+++ src/main/java/com/healthaiharbor/ai/userservice/modal/Role.java
@@ -0,0 +1,41 @@
+package com.healthaiharbor.ai.userservice.modal;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "role_permissions",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id")
+    )
+    private Set<Permission> permissions = new HashSet<>();
+
+    public void addPermission(Permission permission) {
+        this.permissions.add(permission);
+    }
+
+    public void removePermission(Permission permission) {
+        this.permissions.remove(permission);
+    }
+}
