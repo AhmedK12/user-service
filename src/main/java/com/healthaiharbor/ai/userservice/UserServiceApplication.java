0d534268c8fd23bf323237ff679f606ff4b45836
Kamaran Ahmed <kamaran.ahmed@deeptek.ai>
2025-04-05 00:44:26 +0530

    Added native query to extract all username for bloom filter

--- src/main/java/com/healthaiharbor/ai/userservice/UserServiceApplication.java
+++ src/main/java/com/healthaiharbor/ai/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.healthaiharbor.ai.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.healthaiharbor.ai.userservice.mapper")
 public class UserServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/healthaiharbor/ai/userservice/repository/UserRepository.java
+++ src/main/java/com/healthaiharbor/ai/userservice/repository/UserRepository.java
@@ -9,10 +9,13 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
     Optional<User> findByUsername(String username);
+    @Query("SELECT u.username FROM User u")
     List<String> findAllUsernames();
+
 
     boolean existsByUsername(String username);
 }
